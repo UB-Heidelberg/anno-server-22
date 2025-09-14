6870991789b246432b171d870316866a6117d9da
mk-pmb <mk-pmb@users.noreply.github.com>
2025-09-14 17:09:00 +0200

    Censor session IDs when logging request URLs.

--- src/hnd/util/logging.mjs
+++ src/hnd/util/logging.mjs
@@ -5,6 +5,17 @@ import makeRequestSummarizer from 'summarize-express-request-pmb';
 const requestSummarizer = makeRequestSummarizer.logLineArgs();
 
 
+const urlSecretsFilterRgx = (function compile() {
+  const sep = '(?:%[0-9a-f]{2}|\\W)';
+  const maybeSep = sep + '?';
+  const word1 = '(?:s|session)';
+  const word2 = '(?:id|key)';
+  const value = '[\\$\\*-;@-~]+';
+  const rx = ('(' + sep + word1 + maybeSep + word2 + sep + ')' + value);
+  return new RegExp(rx, 'ig');
+}());
+
+
 function logMsg(...args) {
   const d = (new Date()).toISOString();
   console.log(d.slice(0, 10), d.slice(11, 19), ...args);
@@ -23,9 +34,11 @@ function logRequestCheckpoint(where, ...details) {
     (req.complete && '[complete]'),
     (req.res.finished && '[finished]'),
   ].filter(Boolean);
+  let url = String(req.originalUrl || '');
+  url = url.replace(urlSecretsFilterRgx, '$1▒▒▒');
   req.logMsg(where,
     req.method,
-    req.originalUrl,
+    url,
     ...hints,
     ...details);
   return req;
@@ -36,6 +49,7 @@ const EX = {
 
   basics: {
     logMsg,
+    urlSecretsFilterRgx,
   },
 
   middleware: {
