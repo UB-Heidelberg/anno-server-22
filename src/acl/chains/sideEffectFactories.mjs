a47a5bebaade8fc1d94196baa3bbe4081ba34f6a
mk-pmb <mk-pmb@users.noreply.github.com>
2025-11-17 00:12:36 +0100

    ACL: setMetaFromCustomDataDict: Accept priority list as searchFor.

--- src/acl/chains/sideEffectFactories.mjs
+++ src/acl/chains/sideEffectFactories.mjs
@@ -1,6 +1,7 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
 import getOwn from 'getown';
+import mustBe from 'typechecks-pmb/must-be.js';
 import objDive from 'objdive';
 
 import metaSlotTemplate from './metaSlotTemplate.mjs';
@@ -28,14 +29,30 @@ const EX = {
     const { popSpecProp, acl } = how;
     const dict = objDive(acl.initTmp.cfg.customData,
       popSpecProp('nonEmpty str', 'dict'));
-    const searchFor = metaSlotTemplate.compile(
-      popSpecProp('nonEmpty str', 'searchFor'));
+
+    let searchFor = popSpecProp('nonEmpty str | nonEmpty ary', 'searchFor');
+    searchFor = [].concat(searchFor).map(mustBe('nonEmpty str',
+      'Key template for setMetaFromCustomDataDict option "searchFor"'));
+    searchFor = searchFor.map(metaSlotTemplate.compile);
+
+    const debugTrace = popSpecProp('nonEmpty str | undef', 'debugTrace');
     const notFound = popSpecProp('any', 'notFound');
     const setSlot = popSpecProp('nonEmpty str | nul', 'setSlot');
     const pub = popSpecProp('bool | undef', 'public');
+
     return function setMetaFromCustomDataDict(chainCtx) {
-      const key = searchFor(chainCtx);
-      const data = getOwn(dict, key, notFound);
+      let key;
+      let data;
+      searchFor.every(function lookupOneKey(mold) {
+        key = mold(chainCtx);
+        data = getOwn(dict, key);
+        if (debugTrace) {
+          console.debug('setMetaFromCustomDataDict', debugTrace,
+            { mold: mold.origSpec, key, data });
+        }
+        return (data === undefined); // whether to try the next key mold.
+      });
+      if (data === undefined) { data = notFound; }
       if (data === undefined) { return; }
       const { allMeta, pubMeta } = chainCtx;
       if (setSlot === null) {
