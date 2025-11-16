4bba1c952d53619c6302ffe3dadea29a470ea541
mk-pmb <mk-pmb@users.noreply.github.com>
2025-11-16 11:14:20 +0100

    RSS token/key generator: Generate multiple examples.

--- util/create_bcrypt_rss_token.sh
+++ util/create_bcrypt_rss_token.sh
@@ -7,10 +7,15 @@ function create_bcrypt_rss_token () {
   which htpasswd |& grep -qPe '^/' || return $(
     echo 'E: Cannot find htpasswd.' \
       'Try installing apt package apache2-utils.' >&2)
-  local KEY="$(head --bytes=30 -- /dev/urandom | base64 | tr /+ _-)"
-  local HASH="$(htpasswd -inB . <<<"$KEY")"
-  HASH="${HASH#*:}"
-  echo "?key=$KEY     ->     keyHash: '$HASH'"
+  local N_REMAIN="$1"
+  [ "${N_REMAIN:-0}" -ge 1 ] || N_REMAIN=5
+  local KEY= HASH=
+  for (( ; N_REMAIN >= 0 ; N_REMAIN -= 1 )); do
+    KEY="$(head --bytes=30 -- /dev/urandom | base64 | tr /+ _-)"
+    HASH="$(htpasswd -inB . <<<"$KEY")"
+    HASH="${HASH#*:}"
+    echo "keyHash: '$HASH' # ?key=$KEY"
+  done
 }
 
 
