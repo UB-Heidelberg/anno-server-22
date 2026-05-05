39110b72d8641bdc672163cc2e651e79fa76d000
mk-pmb <mk-pmb@users.noreply.github.com>
2026-05-05 20:44:49 +0200

    Disable legacy search.

--- src/hnd/anno/emptyIdGet.mjs
+++ src/hnd/anno/emptyIdGet.mjs
@@ -1,26 +1,13 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-// import debugRequest from '../util/debugRequest.mjs';
 import httpErrors from '../../httpErrors.mjs';
-import legacySearchByTarget from './legacySearchByTarget.mjs';
-import parseVersId from './parseVersionIdentifier.mjs';
 
-const searchNotImpl = httpErrors.notImpl.explain(
-  'Unsupported combination of search criteria.');
-const errNoSuchAnno = httpErrors.noSuchAnno.throwable;
+const deny = httpErrors.badVerb.explain(
+  'Bulk-downloading all annotations is disabled by policy.'
+  + ' Use /anno/by/ to search more specifically.');
 
-
-const EX = async function emptyIdGet(srv, req) {
-  const queryEnts = Object.entries(req.query);
-  const nQuery = queryEnts.length;
-  if (!nQuery) { return parseVersId(errNoSuchAnno); }
-
-  if (nQuery === 1) {
-    const [qk, qv] = queryEnts[0];
-    if (qk === '$target') { return legacySearchByTarget(srv, req, qv); }
-  }
-
-  return searchNotImpl(req);
+const EX = function emptyIdGet(srv, req) {
+  return deny(req);
 };
 
 
